a25d432dffdbe9b8664f36d05659d30b03ea766f
s16446 <s16446@pjwstk.edu.pl>
2020-05-02 21:18:04 +0200

    Dokumentacja SWAGGER + Zadanie 1

--- CW3_WebApplication1/WebApplication1/Startup.cs
+++ CW3_WebApplication1/WebApplication1/Startup.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using WebApplication1.DAL;
+using WebApplication1.Middleware;
 using WebApplication1.Services;
 
 namespace WebApplication1
@@ -30,6 +33,12 @@ namespace WebApplication1
             services.AddTransient<IStudentDbService, SqlServerStudentDbService>();
             services.AddSingleton<IDbService, MockDbService>(); 
             services.AddControllers();
+
+            // 1. Dodawanie dokumentacji
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo{Title="Students App API", Version="v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +48,27 @@ namespace WebApplication1
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Middleware
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                config => { config.SwaggerEndpoint("/swagger/v1/swagger.json","Students App API");
+                    }
+                );
+
+            app.Use(async (context, next) => {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("ERROR [Middleware]: Musisz podaæ numer indeksu");
+                    return;
+                }   
+                await next();
+                
+                
+            });
+            
+            app.UseMiddleware<LoggingMiddleware>();
 
             app.UseHttpsRedirection();
 
